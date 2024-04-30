5d108642eb37bbe978969385d98ffe06d8575c4f
rxuby <jutapon.c@kkumail.com>
2024-04-30 09:42:40 +0700

    [Edit] api connect

--- my-route/src/api/fetchApi.tsx
+++ my-route/src/api/fetchApi.tsx
@@ -4,17 +4,9 @@ import { Button, Modal } from "antd";
 import imgyugi from "../images/pngaaa.com-426287.png";
 import { motion, AnimatePresence } from "framer-motion";
 import "@components/flipcard.css";
-import melffybg from "../images/melffy.png"
+// import melffybg from "../images/melffy.png";
 
 type TypeApi = {
-  // count: string;
-  // next: string;
-  // previous: string;
-  // results: {
-  //   name: string;
-  //   url: string;
-  // }[];
-
   id: number;
   name: string;
   type: "Spell Card" | "Monster Card" | "Trap Cartd";
@@ -30,7 +22,6 @@ type TypeApi = {
 const FetchApi = () => {
   const [data, setData] = useState<TypeApi[]>();
   const [selected, setSelected] = useState<TypeApi | undefined>();
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
@@ -44,79 +35,53 @@ const FetchApi = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  // if(testData)
-  //   const fetchdata = () => {
-  //     fetch('https://pokeapi.co/api/v2/pokemon')
-  //       .then((Response) => {
-  //         if (!Response.ok) {
-  //           throw new Error('Network response was not ok');
-  //         }
-  //         return Response.json();
-  //       })
-  //       .then((data) => {
-  //         setData(data);
-  //       })
-  //       .catch((error) => {
-  //         console.error('There was a problem with the fecth operation', error);
-  //       });
-  //   };
+
   const fetchDataAxios = async () => {
     const result = await axios.get(
       "https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=melffy"
-      // "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=ghost%"
     );
     const _result = result.data.data;
     setData(_result);
-    // axios
-    //   .get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
-    //   .then((response) => {
-    //     setData(response.data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("There was a problem with the fetch operation:", error);
-    //   });
   };
 
   useEffect(() => {
-    // fetchdata();
     fetchDataAxios();
   }, []);
+
   return (
     <AnimatePresence>
-      <motion.div
-      // initial={{ opacity: 0, y: "-100%" }}
-      // animate={{ opacity: 1, y: 0 }}
-      // exit={{ opacity: 0, y: "100%" }}
-      // transition={{ duration: 0.5 }}
-      >
+      <motion.div>
         <div
           style={{ backgroundAttachment: "fixed" }}
           className="flex flex-col justify-center items-center content-center  bg-gradient-to-t from-rose-100 via-rose-100 to-white font-varelaround "
         >
-          {/* <h1>ข้อมูลจาก API</h1> */}
           <div className="  mt-6 mb-6 flex items-center justify-center drop-shadow-xl ">
-            {/* <h1 className="mt-[40px] mb-4 text-[40px] font-semibold  ">Yu-Gi-Oh!</h1> */}
-
-            <img  width={"25%"} src={imgyugi} alt="logo" />
+            <motion.img
+              initial={{ y: -10 }}
+              animate={{ y: [0, -5, 0] }}
+              transition={{ duration: 2, repeat: Infinity }}
+              width={"28%"}
+              src={imgyugi}
+              alt="logo"
+            />
           </div>
-          <div className="bg-white p-12 pl-8 pr-8  rounded-t-[30px] shadow-2xl w-[960px] h-full min-h-screen ">
+          <div className="bg-white p-12 pl-8 pr-8  rounded-t-[30px] shadow-2xl shadow-rose-500/40 w-[960px] h-full min-h-screen ">
             <div className="grid grid-cols-4 grid-flow-row gap-4 ">
               {data &&
                 data.map((item, index) => {
                   return (
                     <motion.div
+                      key={item.id}
                       initial={{ opacity: 0, y: 50 }}
                       animate={{ opacity: 1, y: 0 }}
                       transition={{ delay: 0.5 + index * 0.04 }}
                     >
                       <motion.img
-                        // whileHover={{ scale: 1.1 }}
                         onClick={() => {
                           showModal();
                           setSelected(item);
                         }}
                         className="cursor-pointer hover:scale-[1.1] transition-all rounded-lg"
-                        // width={"15%"}
                         src={item.card_images[0].image_url_cropped}
                         alt={`img-${index}`}
                       />
@@ -126,17 +91,13 @@ const FetchApi = () => {
             </div>
 
             <Modal
-              // title={selected?.name}
               open={isModalOpen}
               onOk={handleOk}
               onCancel={handleCancel}
               footer={null}
               afterClose={() => setSelected(undefined)}
               centered={true}
-              // className="bg-red-500 rounded-lg "
-              style={{
-                backdropFilter: "blur(5px)",
-              }}
+              style={{ backdropFilter: "blur(5px)" }}
             >
               <div className="flex flex-col w-full  ">
                 <h1 className=" font-bold text-[1.5rem] mb-2">
@@ -150,7 +111,6 @@ const FetchApi = () => {
                     alt="image"
                   />
                 </div>
-                {/* <p>{selected?.name}</p> */}
                 <div className="mt-2 ">
                   <p>
                     [{selected?.race} / {selected?.type}]
@@ -162,16 +122,7 @@ const FetchApi = () => {
 
             {data ? (
               <div className="flex">
-                <p>
-                  {/* {data.results.map((item) => {
-              return (
-                <div className="flex flex-col justify-center items-center content-center">
-                  <div className="">ชื่อ: {item.name}</div>
-                  <div>url: {item.url}</div>
-                </div>
-              )
-            })}{" "} */}
-                </p>
+                <p></p>
               </div>
             ) : (
               <p></p>
